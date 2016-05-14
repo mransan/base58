9aaba5b99b3adc884acf0403d0638e031f65e5c2
mransan <maxime.ransan@gmail.com>
2016-05-14 17:38:06 -0400

    Fix example

--- examples/example01.ml
+++ examples/example01.ml
@@ -1,6 +1,6 @@
 let () = 
   let alphabet = B58.make_alphabet "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz" in 
-  let data     = "Hello World"  in 
+  let data     = Bytes.of_string "Hello World"  in 
   let b58      = B58.encode alphabet data in 
   print_endline @@ Bytes.to_string b58
 
